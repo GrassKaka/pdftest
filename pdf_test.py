5649914a148199cf7817361f6c6a4a7b529cd3f5
GrassKaka <rpzheng220807@163.com>
2019-05-07 15:43:04 +0800

    删除无用页面，合成新的pdf文件

--- pdf_test.py
+++ pdf_test.py
@@ -1,8 +1,22 @@
 # coding=utf-8
 
+import os
 import PyPDF2
 
-file_path = 'I:\QQ\QQDownloads\MobileFile\PythonCookbook中文版v3.pdf'
 
-with open(file_path, 'rb') as f:
+file_path = 'I:\QQ\QQDownloads\MobileFile'
+os.chdir(file_path)
+
+file_writer = PyPDF2.PdfFileWriter()
+
+with open('PythonCookbook中文版v3.pdf', 'rb') as f:
     file_reader = PyPDF2.PdfFileReader(f)   # 创建Reader对象
+    for pageNumber in range(file_reader.numPages):
+        pageObj = file_reader.getPage(pageNumber)
+        if 'OpenStack' in pageObj.extractText():
+            continue
+        else:
+            file_writer.addPage(pageObj)
+
+    with open('PythonCookbook中文版v3_updated.pdf', 'wb') as f:
+        file_writer.write(f)
