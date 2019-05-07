0ac70710be3d6b0c959f09aba5e630905533c775
GrassKaka <rpzheng220807@163.com>
2019-05-07 14:58:40 +0800

    创建Reader对象注释

--- pdf_test.py
+++ pdf_test.py
@@ -5,4 +5,4 @@ import PyPDF2
 file_path = 'I:\QQ\QQDownloads\MobileFile\PythonCookbook中文版v3.pdf'
 
 with open(file_path, 'rb') as f:
-    file_reader = PyPDF2.PdfFileReader(f)
+    file_reader = PyPDF2.PdfFileReader(f)   # 创建Reader对象
